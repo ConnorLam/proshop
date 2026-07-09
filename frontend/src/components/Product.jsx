374a9d72921767ed11ab508dc7ae1e9a9798eb19
Connor Lam <connorlam03@yahoo.com>
2026-07-08 17:01:49 -0700

    changed functionality of home screen so whole card could click to enter product screen

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -4,25 +4,23 @@ import Rating from "./Rating"
 
 function Product({ product }) {
   return (
-    <Card className="my-3 p-3 rounded">
-      <Link to={`/product/${product._id}`}>
+    <Link to={`/product/${product._id}`}>
+      <Card className="my-3 p-3 rounded">
         <Card.Img src={product.image} variant="top"></Card.Img>
-      </Link>
-      <Card.Body>
-        <Link to={`/product/${product._id}`}>
+        <Card.Body>
           <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
-        </Link>
-        <Card.Text as="div">
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
-        </Card.Text>
-        <Card.Text as="h3">${product.price}</Card.Text>
-      </Card.Body>
-    </Card>
+          <Card.Text as="div">
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
+          </Card.Text>
+          <Card.Text as="h3">${product.price}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Link>
   )
 }
 

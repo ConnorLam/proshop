351e9f7356054b8becb66fd3d3f4919e2cabb82e
Connor Lam <connorlam03@yahoo.com>
2026-07-14 16:13:42 -0700

    created fetch logic in frontend to fetch from backend created proxy in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react" // Change this to your framework plugin if not using React
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // anything in server has no effect on productions and only in development
+  server: {
+    proxy: {
+      // Intercepts all requests starting with '/api'
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+        // Removes '/api' before sending the request to localhost:5000
+        // rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })

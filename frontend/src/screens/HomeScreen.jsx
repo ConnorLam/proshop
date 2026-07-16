8be357604f3798d02975d452a72b06c58069aad3
Connor Lam <connorlam03@yahoo.com>
2026-07-16 16:40:47 -0700

    added apislice and fetch for get all products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,34 +1,30 @@
-import { useEffect, useState } from "react"
-import { Row, Col } from "react-bootstrap"
-import Product from "../components/Product"
-import axios from "axios"
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 function HomeScreen() {
-  const [products, setProducts] = useState([])
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      // const response = await axios.get('api.products)
-      // const data = response.data
-      const { data } = await axios.get("/api/products")
-      setProducts(data)
-    }
-
-    fetchProducts()
-  }, [])
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
-      <h1>Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;

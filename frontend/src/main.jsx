f1ef559a74d792439af3c46f8ca4c653b88435c3
Connor Lam <connorlam03@yahoo.com>
2026-07-16 15:29:31 -0700

    setup boiler plate redux code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,29 +1,28 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store.js';
 // import "bootstrap/dist/css/bootstrap.min.css"
-import "./assets/styles/bootstrap.custom.css"
-import "./assets/styles/index.css"
-import App from "./App.jsx"
-import HomeScreen from "./screens/HomeScreen.jsx"
-import ProductScreen from "./screens/ProductScreen.jsx"
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+import App from './App.jsx';
+import HomeScreen from './screens/HomeScreen.jsx';
+import ProductScreen from './screens/ProductScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />}></Route>
       <Route path="/product/:id" element={<ProductScreen />}></Route>
-    </Route>,
-  ),
-)
+    </Route>
+  )
+);
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);

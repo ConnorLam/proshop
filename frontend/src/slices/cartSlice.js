a011c7752b5d5b038298f28c9dc7328e4109c5fe
Connor Lam <connorlam03@yahoo.com>
2026-07-17 13:28:15 -0700

    added addtocart function into productscreen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -29,7 +29,7 @@ const cartSlice = createSlice({
       state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
 
       // calc tax price (%15 tax)
-      state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice.toFixed(2)));
+      state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice).toFixed(2));
 
       // calc total price
       state.totalPrice = (Number(state.itemsPrice) + Number(state.shippingPrice) + Number(state.taxPrice)).toFixed(2);

c6999547a440d6234f65533c2591d5d886f60486
Connor Lam <connorlam03@yahoo.com>
2026-07-19 21:07:47 -0700

    started auth user route and created a matchPassword function in userModel to compare what person sends to password in database

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
@@ -21,9 +22,13 @@ const userSchema = new mongoose.Schema(
       default: false,
     },
   },
-  { timestamps: true },
-)
+  { timestamps: true }
+);
 
-const User = mongoose.model("User", userSchema)
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-export default User
+const User = mongoose.model('User', userSchema);
+
+export default User;

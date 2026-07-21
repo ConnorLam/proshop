55ea51cbd0f7b2bc6427d829a53e1ac8575558d2
Connor Lam <connorlam03@yahoo.com>
2026-07-21 15:50:31 -0700

    created auth middleware for admin and protected routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // using es modules instead of commonjs
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -14,6 +15,9 @@ const app = express();
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send('API is running...');

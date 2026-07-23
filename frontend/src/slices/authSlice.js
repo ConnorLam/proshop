c2a25ec89b6ff0fd1181c09b293b225f24b18aba
Connor Lam <connorlam03@yahoo.com>
2026-07-23 13:15:23 -0700

    added logout functionality

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -1,5 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// things that are taken care of on the frontend and not on server like clearing cookies
+
 const initialState = {
   userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null,
 };
@@ -12,9 +14,13 @@ const authSlice = createSlice({
       state.userInfo = action.payload;
       localStorage.setItem('userInfo', JSON.stringify(action.payload));
     },
+    logout: (state, action) => {
+      state.userInfo = null;
+      localStorage.removeItem('userInfo');
+    },
   },
 });
 
-export const { setCredentials } = authSlice.actions;
+export const { setCredentials, logout } = authSlice.actions;
 
 export default authSlice.reducer;

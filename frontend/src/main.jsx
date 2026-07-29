758a9b141e826ee25b74f374968e8120ee42468d
Connor Lam <connorlam03@yahoo.com>
2026-07-28 18:23:49 -0700

    added paypal frontend integration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import store from './store.js';
 // import "bootstrap/dist/css/bootstrap.min.css"
 import './assets/styles/bootstrap.custom.css';
@@ -22,17 +23,17 @@ import OrderScreen from './screens/OrderScreen.jsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<HomeScreen />}/>
-      <Route path="/product/:id" element={<ProductScreen />}/>
-      <Route path="/cart" element={<CartScreen />}/>
-      <Route path="/login" element={<LoginScreen />}/>
-      <Route path="/register" element={<RegisterScreen />}/>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
       <Route path="" element={<PrivateRoute />}>
-        <Route path="/shipping" element={<ShippingScreen />}/>
-        <Route path="/payment" element={<PaymentScreen />}/>
-        <Route path='/placeorder' element={<PlaceOrderScreen />}/>
-        <Route path='/profile' element={<ProfileScreen />}/>
-        <Route path='/order/:id' element={<OrderScreen />}/>
+        <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
+        <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
       </Route>
     </Route>
   )
@@ -41,7 +42,9 @@ const router = createBrowserRouter(
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </StrictMode>
 );

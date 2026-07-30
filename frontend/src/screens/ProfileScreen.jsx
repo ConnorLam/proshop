4725883862982d19ef67ba8d5041fda266015492
Connor Lam <connorlam03@yahoo.com>
2026-07-29 17:55:21 -0700

    added profile screen functionality like updating profile

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,11 +1,99 @@
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { Table, Form, Button, Row, Col } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { useProfileMutation } from '../slices/usersApiSlice';
+import { setCredentials } from '../slices/authSlice';
 
 const ProfileScreen = () => {
-  const user = useSelector((state) => state.auth);
-  console.log(user);
-  const { userInfo } = user;
+  const { userInfo } = useSelector((state) => state.auth);
 
-  return <div>{userInfo.name}</div>;
+  const [name, setName] = useState(userInfo.name);
+  const [email, setEmail] = useState(userInfo.email);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const [updateProfile, { isLoading: loadingUpdateProfile }] = useProfileMutation();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error('Password do not match');
+    } else {
+      try {
+        const res = await updateProfile({ _id: userInfo._id, name, email, password }).unwrap();
+        dispatch(setCredentials(res))
+        toast.success('Profile updated successfully')
+      } catch (err) {
+        toast.error(err?.data.message || err.error);
+      }
+    }
+  };
+  // useEffect(() => {
+  //   if (userInfo){
+  //     setName(userInfo.name);
+  //     setEmail(userInfo.email)
+  //   }
+  // }, [setName, setEmail])
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name" className="my-2">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="email" className="my-2">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="password" className="my-2">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="Password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="confirmPassword" className="my-2">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="Password"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type="submit" variant="primary" className="my-2">
+            Update
+          </Button>
+          {loadingUpdateProfile && <Loader />}
+        </Form>
+      </Col>
+      <Col md={9}>Column</Col>
+    </Row>
+  );
 };
 
 export default ProfileScreen;

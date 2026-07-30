df84db1fec04730f312cb0153c2f0236f052142f
Connor Lam <connorlam03@yahoo.com>
2026-07-29 22:35:20 -0700

    added substring to orderscreen payment to remain consistent

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -117,7 +117,7 @@ const OrderScreen = () => {
                 <strong>Method: </strong> {order.paymentMethod}
               </p>
               {order.isPaid ? (
-                <Message variant="success">Paid on {order.paidAt}</Message>
+                <Message variant="success">Paid on {order.paidAt.substring(0, 10)}</Message>
               ) : (
                 <Message variant="danger">Not Paid</Message>
               )}

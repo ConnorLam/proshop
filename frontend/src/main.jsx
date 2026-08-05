f6529c0be3faf61d0ab083702917269e0c71f151
Connor Lam <connorlam03@yahoo.com>
2026-08-04 17:48:13 -0700

    created product list admin page;

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,6 +21,7 @@ import ProfileScreen from './screens/ProfileScreen.jsx';
 import PlaceOrderScreen from './screens/PlaceOrderScreen.jsx';
 import OrderScreen from './screens/OrderScreen.jsx';
 import OrderListScreen from './screens/admin/OrderListScreen.jsx';
+import ProductListScreen from './screens/admin/ProductListScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -39,6 +40,7 @@ const router = createBrowserRouter(
       </Route>
       <Route path="" element={<AdminRoute />}>
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
+        <Route path="/admin/productlist" element={<ProductListScreen />} />
       </Route>
     </Route>
   )

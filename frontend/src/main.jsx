826c660877b1c8655f28c35a5c777ef2537b9a7a
Connor Lam <connorlam03@yahoo.com>
2026-08-06 15:29:12 -0700

    list users for admin

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -23,6 +23,7 @@ import OrderScreen from './screens/OrderScreen.jsx';
 import OrderListScreen from './screens/admin/OrderListScreen.jsx';
 import ProductListScreen from './screens/admin/ProductListScreen.jsx';
 import ProductEditScreen from './screens/admin/ProductEditScreen.jsx';
+import UserListScreen from './screens/admin/UserListScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -43,6 +44,7 @@ const router = createBrowserRouter(
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
         <Route path="/admin/productlist" element={<ProductListScreen />} />
         <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
+        <Route path="/admin/userlist" element={<UserListScreen />} />
       </Route>
     </Route>
   )

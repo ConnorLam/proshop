e2ba024aedd0a3fafbbec0a9104b398084a4904a
Connor Lam <connorlam03@yahoo.com>
2026-08-06 18:15:22 -0700

    added create review frontend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -109,7 +109,7 @@ const createProductReview = asyncHandler(async (req, res) => {
 
     product.numReviews = product.reviews.length;
 
-    product.rating = product.reviews.reduce((acc, review) => acc + review.length, 0) / product.reviews.length;
+    product.rating = product.reviews.reduce((acc, review) => acc + review.rating, 0) / product.reviews.length;
 
     await product.save();
     res.status(201).json({ message: 'Review added' });
